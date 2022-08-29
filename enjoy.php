2b3d8e78c601f86ef9bfa0f864254489d0c7350f
NINEVPN <103882719+NINEVPN@users.noreply.github.com>
2022-08-29 20:22:55 +0700

    Update enjoy.php

--- enjoy.php
+++ enjoy.php
@@ -1,6 +1,6 @@
 {
   "Version_update": "1.0",
-  "Link_zip": "https://github.com/NINEVPN/begin/blob/main/beginvpn.zip?raw=true",
+  "Link_zip": "https://github.com/NINEVPN/enjoy/raw/main/enjoy.zip",
   "Password": "11111",
   "Message": "ENJOY VPN"
 }
